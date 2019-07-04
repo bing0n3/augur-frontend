309194c013e6774a4f664060df258cccd68c1e6d
bing0n3 <root@bing0ne.com>
2019-07-04 16:17:50 +0800

    convert AugurAPI.js to AugurAPI.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
+import AugurAPI from './AugurAPI';
 Vue.config.productionTip = false;
+
+// interface MyWindow extends Window {
+//   AugurAPI: AugurAPI;
+// }
+// declare let window: MyWindow;
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
 }).$mount('#app');
+
+let a = new AugurAPI('http://localhost:5000');
+
+let r = a.Repo({repo_id: 21000, repo_group_id: 20});
+r.openIssuesCount().then((data:any)=>{
+  console.log(data)
+})
